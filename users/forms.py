7caa86cb29d46dd28d04babd9185cc8f236c4af9
Xavier Ashkar <xaviereight8@gmail.com>
2025-06-04 22:12:03 -0700

    Completed basic register and login pages

---
+++ users/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+class CustomUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'full_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get("password1") != cleaned_data.get("password2"):
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Auto-login after signup
+            return redirect('dashboard')  # You can change this later
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
